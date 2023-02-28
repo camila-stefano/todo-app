c9980335029b31e6b664226a6bb2c728f1d56603
Cami Stefano <cami.stefano@live.com.ar>
2023-02-27 22:23:48 -0300

    delete files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import { useState } from "react";
 import "./App.css";
+import TodoList from "./components/TodoList";
 import data from "./data.json";
+import TodoForm from "./components/TodoForm";
 
 function App() {
   const [todos, setTodos] = useState(data);
 
+  const onComplete = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        return todo.id === Number(id) ? { ...todo, completed: !todo.completed } : { ...todo };
+      })
+    );
+  };
+
+  const onDeleteItem = (id) => {
+    setTodos([...todos].filter((todo) => todo.id !== id));
+  };
+
+  const addTodo = (newTodo) => {
+    console.log("newTodo", newTodo);
+    let newItem = { id: +new Date(), task: newTodo, completed: false };
+    setTodos([...todos, newItem]);
+  };
+
   return (
     <div className="container">
-      <TodoList></TodoList>
+      <TodoForm addTodo={addTodo} />
+      <TodoList todos={todos} onComplete={onComplete} onDeleteItem={onDeleteItem} />
     </div>
   );
 }
